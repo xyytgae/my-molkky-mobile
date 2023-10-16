199ffca296de7b1db3403a6c243926e315ed09e0
xyytgae <xyytgae@yahoo.co.jp>
2023-10-17 01:20:03 +0900

    feat: #1 アプリ名を一括変更

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mymolkkymobile/main.dart';
+import 'package:my_molkky_mobile/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
