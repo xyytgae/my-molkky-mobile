394ad6e7cb7be8e238c43657eb50726f56d5f628
xyytgae <xyytgae@yahoo.co.jp>
2023-10-17 00:39:51 +0900

    feat: 命名等修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Main());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'My Molkky',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const RoomsPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class RoomsPage extends StatefulWidget {
+  const RoomsPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<RoomsPage> createState() => _RoomsPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _RoomsPageState extends State<RoomsPage> {
   int _counter = 0;
 
   void _incrementCounter() {
