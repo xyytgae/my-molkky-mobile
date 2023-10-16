b6a62f55f7c35ac394a04eb73c10bd7faeb0cd82
xyytgae <xyytgae@yahoo.co.jp>
2023-10-17 01:20:48 +0900

    feat: #1 roomsをファイル分割

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_molkky_mobile/ui/page/rooms.dart';
 
 void main() {
   runApp(const Main());
@@ -17,54 +18,6 @@ class Main extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: const RoomsPage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class RoomsPage extends StatefulWidget {
-  const RoomsPage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<RoomsPage> createState() => _RoomsPageState();
-}
-
-class _RoomsPageState extends State<RoomsPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
