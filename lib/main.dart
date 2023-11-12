f83588c83093fe415dadf74caf1c39f7241afed3
xyytgae <xyytgae@yahoo.co.jp>
2023-11-13 02:10:54 +0900

    feat: #1 roomsページl実装

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:my_molkky_mobile/ui/page/rooms.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+final databaseReference = FirebaseFirestore.instance;
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -22,7 +24,30 @@ class Main extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: const RoomsPage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'My Molkky'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final PageController controller = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        children: [
+          Container(color: Colors.white, child: const RoomsPage()),
+        ],
+      ),
     );
   }
 }
--- lib/ui/page/rooms.dart
+++ lib/ui/page/rooms.dart
@@ -1,49 +1,126 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:my_molkky_mobile/model/room.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_molkky_mobile/model/util/color.dart';
 
 class RoomsPage extends StatefulWidget {
-  const RoomsPage({super.key, required this.title});
-
-  final String title;
+  const RoomsPage({super.key});
 
   @override
   State<RoomsPage> createState() => _RoomsPageState();
 }
 
 class _RoomsPageState extends State<RoomsPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    buildRoomList() {
+      final StreamController<List<Room>> allRoomsController =
+          StreamController();
+
+      Stream<List<Room>> getRooms() {
+        final snapshots =
+            FirebaseFirestore.instance.collection('rooms').snapshots();
+        snapshots.listen((snapshot) {
+          final roomList = snapshot.docs.map((DocumentSnapshot doc) {
+            return Room.fromFirestore(doc);
+          }).toList();
+          allRoomsController.add(roomList);
+        });
+
+        return allRoomsController.stream;
+      }
+
+      return StreamBuilder(
+          stream: getRooms(),
+          builder: (BuildContext context, AsyncSnapshot<List<Room>> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              return Column(
+                  children: snapshot.data!.map((data) {
+                return Card(
+                  margin: const EdgeInsets.all(16),
+                  child: ListTile(
+                      title: Text(data.name),
+                      minVerticalPadding: 20,
+                      tileColor: HexColor('#fef5e7'),
+                      trailing: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/room',
+                                arguments: data);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: HexColor('#38512f'),
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Text('入室'))),
+                );
+              }).toList());
+            } else {
+              return const Center(
+                child: Text('Error'),
+              );
+            }
+          });
+    }
+
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('rooms').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Container(
+                alignment: FractionalOffset.center,
+                child: const CircularProgressIndicator());
+          } else {
+            return Scaffold(
+                backgroundColor: HexColor('#f2e4cf'),
+                appBar: AppBar(
+                  leading: IconButton(
+                    icon: const Icon(Icons.menu),
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                  ),
+                  backgroundColor: Colors.white,
+                  title: const Text('My Molkky'),
+                ),
+                body: ListView(
+                  children: [
+                    buildRoomList(),
+                  ],
+                ),
+                bottomNavigationBar: BottomNavigationBar(
+                  fixedColor: HexColor('#38512f'),
+                  items: const [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.home),
+                      label: 'ホーム',
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.add),
+                      label: 'ルーム作成',
+                    ),
+                  ],
+                  currentIndex: 0,
+                  onTap: (int index) {
+                    switch (index) {
+                      case 0:
+                        Navigator.pushNamed(context, '/home');
+                        break;
+                      case 1:
+                        Navigator.pushNamed(context, '/create_room');
+                        break;
+                    }
+                  },
+                ));
+          }
+        });
   }
 }
