cad939b946648a1275506b50493801361d7507f9
xyytgae <xyytgae@yahoo.co.jp>
2024-01-31 01:30:23 +0900

    feat: #6 logoutとprintを実装

--- lib/state/auth_state.dart
+++ lib/state/auth_state.dart
@@ -2,15 +2,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:my_molkky_mobile/model/user.dart';
+import 'package:flutter/services.dart';
 import 'package:my_molkky_mobile/service/user.dart';
 
 class AuthState {
-  User? user;
   UserModel? loginedUser;
   UserRepo userRepo = UserRepo();
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
-  void googleLogin() async {
+  Future<void> googleLogin() async {
+    debugPrint('--------googleLogin--------');
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       if (googleUser == null) {
@@ -27,7 +28,32 @@ class AuthState {
       final user =
           (await FirebaseAuth.instance.signInWithCredential(credential)).user;
       loginedUser = await userRepo.get(user!.uid);
+    } on PlatformException catch (error) {
+      debugPrint('--PlatformException--');
+      debugPrint('error: $error');
+    } on Exception catch (error) {
+      debugPrint('--Exception--');
+      debugPrint('error: $error');
     } catch (error) {
+      debugPrint('--catch--');
+      debugPrint('error: $error');
+    }
+  }
+
+  Future<void> logout() async {
+    debugPrint('--------logout--------');
+    try {
+      await _googleSignIn.signOut();
+      await FirebaseAuth.instance.signOut();
+      loginedUser = null;
+    } on PlatformException catch (error) {
+      debugPrint('--PlatformException--');
+      debugPrint('error: $error');
+    } on Exception catch (error) {
+      debugPrint('--Exception--');
+      debugPrint('error: $error');
+    } catch (error) {
+      debugPrint('--catch--');
       debugPrint('error: $error');
     }
   }
