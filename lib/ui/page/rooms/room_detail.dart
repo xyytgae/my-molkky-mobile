5c92ca52dd7fb6118953dc1d1d560235e72b74c5
xyytgae <xyytgae@yahoo.co.jp>
2024-02-14 01:36:49 +0900

    feat: #11 room入室時の処理実装

--- lib/ui/page/rooms/room_detail.dart
+++ lib/ui/page/rooms/room_detail.dart
@@ -4,6 +4,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:my_molkky_mobile/model/room.dart';
 import 'package:my_molkky_mobile/model/player.dart';
 import 'package:my_molkky_mobile/model/util/color.dart';
+import 'package:my_molkky_mobile/service/room.dart';
+import 'package:my_molkky_mobile/service/player.dart';
+import 'package:my_molkky_mobile/state/auth_state.dart';
+import 'package:provider/provider.dart';
 
 class RoomDetailPage extends StatefulWidget {
   final String roomId;
@@ -16,6 +20,9 @@ class RoomDetailPage extends StatefulWidget {
 class _RoomDetailPageState extends State<RoomDetailPage> {
   String roomId = '';
 
+  final RoomRepo roomRepo = RoomRepo();
+  final PlayerRepo playerRepo = PlayerRepo();
+
   @override
   void initState() {
     super.initState();
@@ -24,9 +31,29 @@ class _RoomDetailPageState extends State<RoomDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    buildRoomDetail() {
+    final state = Provider.of<AuthState>(context, listen: true);
+
+    buildRoomDetail(Room? room) {
       final StreamController<List<Player>> playerController =
           StreamController();
+
+      init() async {
+        if (state.loginedUser != null &&
+            room != null &&
+            room.playerIds.length < 4) {
+          await roomRepo.enterRoom(roomId, state.loginedUser!.id);
+          await playerRepo.create(
+              roomId,
+              Player(
+                  id: state.loginedUser!.id,
+                  name: state.loginedUser!.name,
+                  iconImageUrl: state.loginedUser!.iconImageUrl,
+                  stars: state.loginedUser!.stars,
+                  order: 0));
+        }
+      }
+
+      init();
 
       Stream<List<Player>> getPlayers() {
         final snapshots = FirebaseFirestore.instance
@@ -90,17 +117,8 @@ class _RoomDetailPageState extends State<RoomDetailPage> {
           });
     }
 
-    Future<Room> getRoom() async {
-      final document = await FirebaseFirestore.instance
-          .collection('rooms')
-          .doc(roomId)
-          .get();
-
-      return Room.fromDocument(document);
-    }
-
     return FutureBuilder(
-      future: getRoom(),
+      future: roomRepo.get(roomId),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return Container(
@@ -116,7 +134,7 @@ class _RoomDetailPageState extends State<RoomDetailPage> {
             ),
             body: Stack(
               children: [
-                buildRoomDetail(),
+                buildRoomDetail(snapshot.data),
                 Container(
                   margin: const EdgeInsets.only(bottom: 10.0),
                   child: Align(
